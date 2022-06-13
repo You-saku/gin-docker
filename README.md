338b26c4cd75585c215b30d6a7ad96e4578c854b
You-saku <yousaku.dev19@gmail.com>
2022-06-14 01:24:02 +0900

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# gin-docker
+Gin(Golang)環境を作る。
+
+## 構成
+```
+golang 1.17.6
+```
+
+## セットアップ
+```
+git clone 
+docker-comose up -d --build
+```
+
+## 入ってるgo.mod一覧
+```
+go get -u gorm.io/gorm
+go get -u gorm.io/driver/mysql
+go get -u github.com/gin-gonic/gin
+```
---
+++ server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	// Gin用
+	"net/http"
+	"github.com/gin-gonic/gin"
+
+	// gorm用
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	/*
+	* DB接続確認
+	* Todo: DBへの接続情報は環境変数で管理
+	*/
+	user := "user"
+    pass := "secret"
+    protocol := "tcp(db:3306)" // 文法 => tco(コンテナ名:ポート番号) ハマるポイント
+    dbName := "develop"
+
+
+	type Sample struct {
+		gorm.Model
+		Code  string
+		Price uint
+	}
+	connect := user + ":" + pass + "@" + protocol + "/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(connect), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&Sample{})
+
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello gin!!",
+		})
+	})
+
+	r.Run(":80")
+}
